31ab5a340224dcb047e36de1818e9cc81171e2e7
Sascha Drux <sascha.drux@haw-hamburg.de>
2012-07-18 18:15:08 +0200

    test

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();				# Sicher gehen, dass es eine Session gibt
-	session_destroy();				# Session schliessen
-	header("location:index.php");	# zum Index zurueckkehren
-	exit();
-?>
